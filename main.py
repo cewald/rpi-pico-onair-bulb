4273f7bce1f36e8e46bf73eab5a64c347df3a836
cewald <hello@christianewald.de>
2024-09-21 12:38:29 +0200

    Remove hostname change

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import network
-import usocket as socket
 from time import sleep
 from machine import Pin
 
@@ -7,8 +6,8 @@ from machine import Pin
 pin = Pin("LED", Pin.OUT)
 
 # Set up your Wi-Fi connection
-ssid = 'YourSSID'
-password = 'YourPassword'
+ssid = 'Your Wifi name'
+password = 'XXXXXXXX'
 
 # Connect to Wi-Fi
 wlan = network.WLAN(network.STA_IF)
@@ -23,10 +22,4 @@ while not wlan.isconnected():
 
 print('Connected to WiFi:', wlan.ifconfig())
 
-# Workaround: Set the hostname (might need specific configuration or router support)
-custom_hostname = 'on-air-bulb'
-addr_info = socket.getaddrinfo(custom_hostname, 80)
-
-print(f"Hostname set to {custom_hostname} with address info:", addr_info)
-
-pin.toggle()
+pin.on()
