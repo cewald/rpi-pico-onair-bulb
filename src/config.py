326cae4b8f7d58e1174abd544d808be65df8cfcc
cewald <hello@christianewald.de>
2024-09-21 18:29:21 +0200

    feat: improve configs

--- src/config.py
+++ src/config.py
@@ -1,13 +1,11 @@
 import ujson
 
-data = ''
-
-def load_config():
-    global data
-    with open("config.json", "r") as file:
-        data = ujson.load(file) 
-    return data
+data = None
 
 def get_config():
+    global data
+    if not data:
+        print('Init config')
+        with open("config.json", "r") as file:
+            data = ujson.load(file) 
     return data
-
--- src/wifi.py
+++ src/wifi.py
@@ -1,12 +1,14 @@
 import network
 from time import sleep
-from src.config import load_config
+from src.config import get_config
 
-config = load_config()
+config = get_config()
 wlan = network.WLAN(network.STA_IF)
 
 def connect_to_wifi(ssid = config['wifiName'], password = config['wifiPassword']):
     wlan.active(True)
+
+    get_config()
 
     network.hostname(config['hostname'])
     wlan.connect(ssid, password)
