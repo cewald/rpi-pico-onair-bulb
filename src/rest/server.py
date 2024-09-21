e8c342f7082e6e03dca602fe9f63b64e25b0aabd
cewald <hello@christianewald.de>
2024-09-21 19:53:14 +0200

    feat: add rest endpoints

--- src/rest/server.py
+++ src/rest/server.py
@@ -1,4 +1,5 @@
 from src.microdot import Microdot
+from machine import Pin
 from src.config import getConfig
 
 print('Init server')
@@ -6,12 +7,32 @@ print('Init server')
 app = Microdot()
 config = getConfig()
 
+led = Pin('LED', Pin.OUT)
+
 def startServer():
     port = config['port']
     print('Start server on port:', port)
     app.run(port=port)
 
-@app.route('/')
-async def index(request):
-    return 'Hello, world!'
+def returnResponse(success=True):
+    return {'success': True, 'state': led.value()}
 
+@app.route('/', methods=['GET'])
+async def index(request):
+    return returnResponse()
+
+@app.route('/on', methods=['GET'])
+async def on(request):
+    led.on()
+    return returnResponse()
+
+@app.route('/off', methods=['GET'])
+async def off(request):
+    led.off()
+    return returnResponse()
+
+
+@app.route('/toggle', methods=['GET'])
+async def toggle(request):
+    led.toggle()
+    return returnResponse()
