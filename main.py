bf67f4cfea1cd6f5bad97949326c6c1695be15f6
cewald <hello@christianewald.de>
2024-09-21 12:14:29 +0200

    chore: change hostname

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ while not wlan.isconnected():
 print('Connected to WiFi:', wlan.ifconfig())
 
 # Workaround: Set the hostname (might need specific configuration or router support)
-custom_hostname = 'my-pico-w'
+custom_hostname = 'on-air-bulb'
 addr_info = socket.getaddrinfo(custom_hostname, 80)
 
 print(f"Hostname set to {custom_hostname} with address info:", addr_info)
