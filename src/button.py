ed65d9659d4f56e56d4a20582d99679bc0c04cbf
cewald <hello@christianewald.de>
2024-09-22 12:32:44 +0200

    feat: prepare button

---
+++ src/button.py
@@ -0,0 +1,23 @@
+from time import sleep
+from machine import Pin
+from src.display import display
+
+button = Pin(6, Pin.IN, Pin.PULL_DOWN)
+current_value = button.value()
+
+
+async def init_button():
+    print("Init button")
+
+    global current_value
+    while True:
+        print("Button:", button.value())
+        if button.value() != current_value:
+            print("Button pressed:", button.value())
+            current_value = button.value()
+            if current_value == 1:
+                display.draw_bitmap()
+            else:
+                display.clear()
+
+        sleep(0.1)
