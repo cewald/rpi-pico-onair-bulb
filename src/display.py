96d87b94307cda39cde0f086238f612fc2714464
cewald <c.ewald@spiri.bo>
2024-09-25 12:23:47 +0200

    feat: improve scroll-animation

--- src/display.py
+++ src/display.py
@@ -1,4 +1,4 @@
-from asyncio import create_task, sleep
+from asyncio import Event, create_task, sleep, CancelledError
 from machine import Pin, SPI
 from lib.max7219 import Matrix8x8
 from src.image import image_one, image_zero
@@ -9,11 +9,11 @@ class Display(object):
         print("Init display")
 
         self.text = ""
-        self.run_bounce_animation = True
         self.matrix_count = 4
         self.spi = SPI(0, sck=Pin(2), mosi=Pin(3))
         self.cs = Pin(5, Pin.OUT)
         self.display = Matrix8x8(self.spi, self.cs, self.matrix_count)
+        self.bounce_task = None
 
         self.display.brightness(0)
         self.display.fill(0)
@@ -28,33 +28,40 @@ class Display(object):
         self.text = text
 
         if len(text) > 4:
-            create_task(self.bounce_text(text=text, delay=delay))
+            self.bounce_task = create_task(self.bounce_text(text=text, delay=delay))
         else:
             self.display.text(text, 0, 0, 1)
             self.display.show()
 
-    async def bounce_text(self, text, delay=0.1):
-        self.run_bounce_animation = False
-        self.run_bounce_animation = True
+    async def bounce_text(self, text, delay=0.1, end_wait=0.5):
+        print('bounce_text', text)
 
         length = len(text) * 8
         max_offset = length - 32
         direction = 1
         pos = 0
+        
+        switch = False
+        is_initialized = False
 
-        while self.run_bounce_animation:
+        while True:
             self.display.fill(0)
             self.display.text(text, -pos, 0, 1)
             self.display.show()
 
             await sleep(delay)
 
+            if switch or not is_initialized:
+                await sleep(end_wait)
+                switch = False
+                is_initialized = True
+
             pos += direction
 
             if pos >= max_offset or pos <= 0:
                 direction *= -1
+                switch = True
 
-        print("Debounce")
 
     def draw_image(self, image_byte_array=bytearray([])):
         for y in range(8):
@@ -75,8 +82,16 @@ class Display(object):
         global image_one
         self.draw_image(image_zero)
 
+    def stop_bounce_text(self):
+        if self.bounce_task and not self.bounce_task.done():
+            self.bounce_task.cancel()
+
+        self.bounce_task = None
+
     def clear(self):
         self.text = ""
+        self.stop_bounce_text()
+
         self.display.fill(0)
         self.display.show()
 
