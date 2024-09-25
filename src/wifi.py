31dd520e9b729509e8fb18a13517b3f6f0b082e9
cewald <c.ewald@spiri.bo>
2024-09-25 11:52:18 +0200

    feat: add timeout to wifi

--- src/wifi.py
+++ src/wifi.py
@@ -13,12 +13,14 @@ def connect_to_wifi(ssid=config["wifiName"], password=config["wifiPassword"]):
     network.hostname(config["hostname"])
     wlan.connect(ssid, password)
 
+    max_wait = 20
+
     print("Connecting to WiFi")
     display.show_text("init")
     sleep(0.5)
 
     count = 0
-    while not wlan.isconnected():
+    while not wlan.isconnected() and max_wait > 0:
         print(".", end="")
 
         count = count + 1 if count < 4 else 1
@@ -27,6 +29,7 @@ def connect_to_wifi(ssid=config["wifiName"], password=config["wifiPassword"]):
             text = text + "."
         display.show_text(text)
 
+        max_wait -= 1
         sleep(1)
 
     print("\nConnected to WiFi:", wlan.ifconfig()[0])
